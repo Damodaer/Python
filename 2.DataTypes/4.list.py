e24417ce84fb32389c3072e682be796fba2f16d0
Damadaer <damodarsahuofficial94@gmail.com>
2024-04-30 00:24:16 +0530

    initial commit

---
+++ 2.DataTypes/4.list.py
@@ -0,0 +1,38 @@
+# list = ["Damo","dar","Sahu"]
+# print(list)
+
+# em_list = []
+# print("Blank list: ")
+# print(em_list)
+# print(type(em_list))
+
+# num = [10, 54, 40,]
+# print("\nList of Numbers: ")
+# print(num)
+
+# var = ["Odisha","Ganjam","Berhampur"]
+# print("\nList of Names: ")
+# print(var)
+
+# duplicate elements---
+
+# List = [1,2,3,4,5,6,7,5,4,6,6,5,5]
+# print("\nList with the use of Numbers: ") 
+# print(List) 
+
+# mixed type of values
+
+# List2 = [1, "Damodar", 3,"AHuti",5,"Sahu"]
+# print("\nList with the use of Mixed Values: ") 
+# print(List2)
+
+# Accessing elements from list---
+
+names = ["Ahuti","Damodar","Sai","Alekha","Loku"]
+print(names)
+print(len(names))
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
