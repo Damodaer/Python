99cbe3b9b37a95987e4a1a4b74a49a5677dd3811
Damadaer <damodarsahuofficial94@gmail.com>
2024-04-30 22:43:52 +0530

    initial commit

--- 2.DataTypes/4.list.py
+++ 2.DataTypes/4.list.py
@@ -28,11 +28,42 @@
 
 # Accessing elements from list---
 
-names = ["Ahuti","Damodar","Sai","Alekha","Loku"]
-print(names)
-print(len(names))
-print(names[0])
-print(names[1])
-print(names[2])
-print(names[3])
-print(names[4])
+# names = ["Ahuti","Damodar","Sai","Alekha","Loku"]
+# print(names)
+# print(len(names))
+# print(names[0])
+# print(names[1])
+# print(names[2])
+# print(names[3])
+# print(names[4])
+# print(names[-1])
+# print(names[-2])
+# print(names[-3])
+# print(names[-4])
+# print(names[-5])
+
+
+
+# Nesting list---
+
+# name_list =  [["Santinagar","girisola","surola road"],["junction"]]
+
+# print(name_list)
+# print("\nAccessing a element from Nested List: ")
+# print(name_list[1])
+# print(name_list[0][1])
+# print(name_list[1][0])
+
+
+# Taking Input of a Python List---
+
+# string = input("Enter elements (Space-Separated):")
+
+# lst = string.split()
+# print('The list is:',lst)
+
+
+name = input("Enter Names (space-separated):")
+
+# name_list = name.split()
+print(name)
